5d5b654185535fc2ea08bfbc8b5f7645ffe19889
oat431 <oat431@gmail.com>
2023-08-12 18:50:36 +0700

    🛠 setup: personal practice project

---
+++ src/main/java/panomete/project/cometopimabe/ComeToPiMaBeApplication.java
@@ -0,0 +1,13 @@
+package panomete.project.cometopimabe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComeToPiMaBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComeToPiMaBeApplication.class, args);
+    }
+
+}
